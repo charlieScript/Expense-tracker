6fb4e8ee00a4edd994a4f998a2b4804f0c568503
charieScript <gozione3@gmail.com>
2020-07-12 21:04:25 +0100

    added my tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div>
             <h2>Expense Tracker</h2>
-            <p className='header'>A basic expense tracker</p>
+            <p className='header'>A basic expense tracker Built by charliescript. Made with React</p>
         </div>
     )
 }
