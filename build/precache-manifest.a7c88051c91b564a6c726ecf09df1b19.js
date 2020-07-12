568bd1ff9b1f96cc1cbe57443ebfda1d3eb99678
charieScript <gozione3@gmail.com>
2020-07-12 21:08:07 +0100

    added build folder

---
+++ build/precache-manifest.a7c88051c91b564a6c726ecf09df1b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df356a90ff2f5c40a5088fb20a4a6c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30ff2289190c0d983d67",
+    "url": "/static/css/main.f88775c3.chunk.css"
+  },
+  {
+    "revision": "d72e1cb39b8a0abf2e1b",
+    "url": "/static/js/2.52bf4b82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52bf4b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ff2289190c0d983d67",
+    "url": "/static/js/main.628581dd.chunk.js"
+  },
+  {
+    "revision": "7377c01af026f805617c",
+    "url": "/static/js/runtime-main.13522d88.js"
+  }
+]);
