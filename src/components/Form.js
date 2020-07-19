cf57e034e4942c11db5b191e2366fdea18b0e9d6
qudusayo <zeekimono@gmail.com>
2020-07-19 05:03:14 +0100

    Cleanup and Reafactored

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import './stylesheet/Form.css';
-import Expense from './Expense';
-import localstorageFunctions from './LocalStorage';
+import React, { Component } from "react";
+import "./stylesheet/Form.css";
+import Expense from "./Expense";
+import localstorageFunctions from "./LocalStorage";
 // holds the basic data
 const dataStr = [];
 let newDataStr, newTotal;
@@ -12,45 +12,27 @@ class Form extends Component {
 
     this.state = {
       data: [],
-      amount: '',
-      to: '',
-      note: '',
-      date: '',
+      amount: "",
+      to: "",
+      note: "",
+      date: "",
       total: 0,
     };
+
+    this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount() {
-    newDataStr = localstorageFunctions.getData('expense');
-    newTotal = localstorageFunctions.getData('total');
+    newDataStr = localstorageFunctions.getData("expense");
+    newTotal = localstorageFunctions.getData("total");
     this.setState({
       data: newDataStr,
       total: newTotal,
     });
   }
 
-  handleAmount = (e) => {
-    this.setState({
-      amount: e.target.value,
-    });
-  };
-
-  handleTo = (e) => {
-    this.setState({
-      to: e.target.value,
-    });
-  };
-
-  handleNote = (e) => {
-    this.setState({
-      note: e.target.value,
-    });
-  };
-
-  handleDate = (e) => {
-    this.setState({
-      date: e.target.value,
-    });
+  onChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
   };
 
   handleSubmit = (e) => {
@@ -67,12 +49,12 @@ class Form extends Component {
     this.setState(
       {
         data: dataStr,
-        amount: '',
-        to: '',
-        note: '',
-        date: '',
+        amount: "",
+        to: "",
+        note: "",
+        date: "",
       },
-      localstorageFunctions.storeData('expense', dataStr),
+      localstorageFunctions.storeData("expense", dataStr)
     );
     this.getTotal();
   };
@@ -86,7 +68,7 @@ class Form extends Component {
       {
         total: initialValue,
       },
-      localStorage.setItem('total', initialValue),
+      localStorage.setItem("total", initialValue)
     );
   };
 
@@ -94,10 +76,10 @@ class Form extends Component {
     this.setState(
       {
         data: [],
-        total: '',
+        total: "",
       },
-      localstorageFunctions.clearData('expense'),
-      localStorage.removeItem('total'),
+      localstorageFunctions.clearData("expense"),
+      localStorage.removeItem("total")
     );
   };
 
@@ -108,77 +90,77 @@ class Form extends Component {
         ))
       : null;
     return (
-      <div>
-        <div className="card">
-          <section>
-            <form onSubmit={this.handleSubmit}>
-              <div className="input-group">
-                <label htmlFor="amount">Amount</label>
-                <input
-                  onChange={this.handleAmount}
-                  value={this.state.amount}
-                  type="number"
-                  id="amount"
-                  required
-                />
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="to">To</label>
-                <input
-                  type="text"
-                  value={this.state.to}
-                  onChange={this.handleTo}
-                  id="to"
-                  required
-                />
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="note">Note</label>
-                <textarea
-                  id="note"
-                  onChange={this.handleNote}
-                  cols="10"
-                  rows="5"
-                  required
-                  value={this.state.note}
-                ></textarea>
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="date">Date</label>
-                <input
-                  type="date"
-                  value={this.state.date}
-                  onChange={this.handleDate}
-                  id="date"
-                  required
-                />
-              </div>
-
-              <input type="submit" value="Add expense" />
-            </form>
-            <div className="functions">
-              <button onClick={this.clearExp} className="clear">
-                Clear Expense
-              </button>
+      <div className="card">
+        <section>
+          <form onSubmit={this.handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="amount">Amount</label>
+              <input
+                autoComplete="off"
+                onChange={this.onChange}
+                value={this.state.amount}
+                type="number"
+                id="amount"
+                required
+              />
             </div>
-          </section>
-          <aside>
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                borderBottom: '1px solid grey',
-              }}
-            >
-              <h3>Expenses</h3>
-              <p className="total">Total Amount: #{this.state.total} </p>
+
+            <div className="input-group">
+              <label htmlFor="to">To</label>
+              <input
+                autoComplete="off"
+                type="text"
+                value={this.state.to}
+                onChange={this.onChange}
+                id="to"
+                required
+              />
             </div>
-            {expenseList}
-          </aside>
+
+            <div className="input-group">
+              <label htmlFor="note">Note</label>
+              <textarea
+                id="note"
+                onChange={this.onChange}
+                cols="10"
+                rows="5"
+                required
+                value={this.state.note}
+              ></textarea>
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                value={this.state.date}
+                onChange={this.onChange}
+                id="date"
+                required
+              />
+            </div>
+
+            <input type="submit" value="Add expense" />
+          </form>
+          <div className="functions">
+            <button onClick={this.clearExp} className="clear">
+              Clear Expense
+            </button>
+          </div>
+        </section>
+        <div className="aside">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              borderBottom: "1px solid grey",
+            }}
+          >
+            <h3>Expenses</h3>
+            <p className="total">Total Amount: #{this.state.total} </p>
+          </div>
+          {expenseList}
         </div>
       </div>
     );
